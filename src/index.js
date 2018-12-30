9a1e5f7f8fe28c675c86477a7d5ea411150d3127
Nicole Alli <alli.nicolette@gmail.com>
2018-12-29 23:24:20 -0500

    added gh-pages package

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Main from "./main";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fab);
 
-import Main from "./main";
-
 ReactDOM.render(
-  <HashRouter><Main/></HashRouter>,
+  <HashRouter basename={process.env.PUBLIC_URL}><Main/></HashRouter>,
   document.getElementById("root")
 );
